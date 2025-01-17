2346aa0e1c59a2e51ee6e72d4d1a1439361c1a46
Alexarcelo <marcelo@luckreceptivo.com.br>
2025-01-17 14:14:21 -0300

    Update Paxs_IN_OUT_Passeios.py

--- Paxs_IN_OUT_Passeios.py
+++ Paxs_IN_OUT_Passeios.py
@@ -58,26 +58,10 @@ def puxar_dados_phoenix(base_luck):
 
 def puxar_aba_simples(id_gsheet, nome_aba, nome_df):
 
-    # GCP projeto onde está a chave credencial
-    project_id = "grupoluck"
-
-    # ID da chave credencial do google.
-    secret_id = "cred-luck-aracaju"
-
-    # Cria o cliente.
-    secret_client = secretmanager.SecretManagerServiceClient()
-
-    secret_name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
-    response = secret_client.access_secret_version(request={"name": secret_name})
-
-    secret_payload = response.payload.data.decode("UTF-8")
-
-    credentials_info = json.loads(secret_payload)
-
-    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-
-    # Use the credentials to authorize the gspread client
-    credentials = Credentials.from_service_account_info(credentials_info, scopes=scopes)
+    nome_credencial = st.secrets["CREDENCIAL_SHEETS"]
+    credentials = service_account.Credentials.from_service_account_info(nome_credencial)
+    scope = ['https://www.googleapis.com/auth/spreadsheets']
+    credentials = credentials.with_scopes(scope)
     client = gspread.authorize(credentials)
 
     spreadsheet = client.open_by_key(id_gsheet)
@@ -90,26 +74,10 @@ def puxar_aba_simples(id_gsheet, nome_aba, nome_df):
 
 def inserir_config(df_itens_faltantes, id_gsheet, nome_aba):
 
-    # GCP projeto onde está a chave credencial
-    project_id = "grupoluck"
-
-    # ID da chave credencial do google.
-    secret_id = "cred-luck-aracaju"
-
-    # Cria o cliente.
-    secret_client = secretmanager.SecretManagerServiceClient()
-
-    secret_name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
-    response = secret_client.access_secret_version(request={"name": secret_name})
-
-    secret_payload = response.payload.data.decode("UTF-8")
-
-    credentials_info = json.loads(secret_payload)
-
-    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-
-    # Use the credentials to authorize the gspread client
-    credentials = Credentials.from_service_account_info(credentials_info, scopes=scopes)
+    nome_credencial = st.secrets["CREDENCIAL_SHEETS"]
+    credentials = service_account.Credentials.from_service_account_info(nome_credencial)
+    scope = ['https://www.googleapis.com/auth/spreadsheets']
+    credentials = credentials.with_scopes(scope)
     client = gspread.authorize(credentials)
     
     spreadsheet = client.open_by_key(id_gsheet)
